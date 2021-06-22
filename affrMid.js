cf9b76eddc5ceec20b5bde0c58abb980ef25032f
ks-sup <iiizotovak@gmail.com>
2021-06-23 00:07:03 +0300

    font

--- affrMid.js
+++ affrMid.js
@@ -3,6 +3,10 @@ function generateM() {
     '<img src="images/imgMid/middle1.svg">',
     '<img src="images/imgMid/middle2.svg">',
     '<img src="images/imgMid/middle3.svg">',
+    '<img src="images/imgMid/middle4.svg">',
+    '<img src="images/imgMid/middle5.svg">',
+    '<img src="images/imgMid/middle6.svg">',
+    
 ];
 
 var para=document.getElementById("div2");
